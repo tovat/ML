09931113a0618f09621dbca33a7c8d5a52dbdc5d
tovat <147144672+tovat@users.noreply.github.com>
2024-03-20 09:27:09 +0100

    Made changes to model

--- model.py
+++ model.py
@@ -13,19 +13,8 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 joblib.dump(scaler, ("scaler.pkl"))
 
-# Fitting the final model to the whole dataset
+# Fitting the final model to the whole dataset and saving it
 final_model = SVC(C=0.5, gamma=1, kernel='poly')
 final_model.fit(X_scaled, y)
-
-#model = joblib.load("C:/Users/tovat/OneDrive/Dokument/EC_utbildning/MachineLearning/final_model.pkl")
-
-
-# Save the final model to a temporary location
-#temp_model_path = "temp_final_model.pkl"
-#joblib.dump(final_model, temp_model_path)
-
-# Move the final model to the large file storage area
-#os.system("git lfs track 'final_model.pkl'")
-#os.system(f"mv {temp_model_path} final_model.pkl")
 
 joblib.dump(final_model, ("final_model.pkl"))
